cb39e9de0caa2c2778868920af445ab8913acd3c
Göksel Köksal <gokselkoksal@gmail.com>
2018-11-25 01:06:32 +0000

    Initial Commit

---
+++ NetworkModule/NetworkModule.h
@@ -0,0 +1,19 @@
+//
+//  NetworkModule.h
+//  NetworkModule
+//
+//  Created by Göksel Köksal on 25.11.2018.
+//  Copyright © 2018 GK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkModule.
+FOUNDATION_EXPORT double NetworkModuleVersionNumber;
+
+//! Project version string for NetworkModule.
+FOUNDATION_EXPORT const unsigned char NetworkModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkModule/PublicHeader.h>
+
+
